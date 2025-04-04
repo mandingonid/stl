8d47c5983d458de0c2d8e76f6f0819957fa256aa
Rodney Moore <rodney@nerdsindenial.com>
2025-04-04 01:33:53 -0700

    Initial draft of standard template library set template.

---
+++ STLSETS/set_template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+
+int main(int argc, char *argvp[]){
+	
+	int result= -1;
+	std::set<int> setA{1, 2, 3, 5, 7, 11, 5};
+	setA.insert(1);
+	setA.insert(2);
+	setA.insert(-13);
+	
+	std::cout << "5?: " << setA.count(5) << std::endl;
+	std::cout << "5? " << setA.contains(5) << std::endl;
+	
+	auto found = setA.find(7);
+	if(found != setA.end()){
+		for(auto index: setA)
+			std::cout << index << std::endl;
+		std::cout << "We guess " << *found <<  " is no longer lost" << std::endl;
+		setA.erase(*found);
+		std::cout << "Changed our minds and erased " << *found << std::endl;
+		for(auto index: setA)
+			std::cout << index << std::endl;
+		result = 0;
+	}
+	return result;
+	
+}
