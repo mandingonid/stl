f63a929f469c3260f907f37886d255d0a5210ea0
Rodney Moore <rodney@nerdsindenial.com>
2026-05-23 07:26:03 -0700

    Changed stack values to prime numbers  On branch STLBASICS20260525  Your branch is up to date with 'origin/REMOSTLBASICS20260525'. 	modified:   BasicStack.cpp

--- STLBASICS/BasicStack.cpp
+++ STLBASICS/BasicStack.cpp
@@ -12,9 +12,17 @@ int main() {
     std::stack<int> myStack;
 
     // Push elements onto the stack
-    myStack.push(10);
-    myStack.push(20);
-    myStack.push(30);
+    myStack.push(1);
+    myStack.push(3);
+    myStack.push(5);
+    myStack.push(7);
+    myStack.push(11);
+    myStack.push(13);
+    myStack.push(17);
+    myStack.push(19);
+    myStack.push(23);
+    myStack.push(29);
+    myStack.push(31); 
 
     // Display the top element
     std::cout << "Top element: " << myStack.top() << std::endl;
