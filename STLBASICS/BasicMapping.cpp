59a8f4eef6364c383f5de2e7c9ebed82260f1a16
Rodney Moore <rodney@nerdsindenial.com>
2026-05-23 09:29:27 -0700

    Fixed typo from "game data" to "player data"  On branch STLBASICS20260525  Your branch is up to date with 'origin/REMOSTLBASICS20260525'. 	modified:   BasicMapping.cpp

--- STLBASICS/BasicMapping.cpp
+++ STLBASICS/BasicMapping.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <unordered_map>
 #include <string>
 
 
@@ -20,7 +21,7 @@ int main() {
   std::map<std::string, int> playerRoster;
 
   if(playerFile.is_open()){
-    std::cout << "This file will be used to store game data." << std::endl;
+    std::cout << "This file will be used to store player data." << std::endl;
     std::getline(playerFile, playerLine);  // read and throw away file header line
     std::getline(playerFile, playerLine);  // read and throw away file column headers
 
@@ -35,6 +36,10 @@ int main() {
 
       playerRoster.insert(std::pair<std::string, int>(firstName + " " + lastName, std::stoi(playerRank)));
     }
+
+    playerRoster["Macklin Celebrini"] = 10; // Adding a player to the roster using operator[]
+    // playerRoster.erase("Macklin Celebrini"); // Removing a player from the roster
+    // playerRoster.clear(); // Clearing the entire roster
 
     for(auto pair : playerRoster){
       std::cout << "Player: " << pair.first << ", Rank: " << pair.second << std::endl;
