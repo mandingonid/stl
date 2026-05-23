e4176be162083c3b5af52afc839b6bf6bff5b467
Rodney Moore <rodney@nerdsindenial.com>
2026-05-23 08:17:39 -0700

    On branch STLBASICS20260525  Your branch is up to date with 'origin/REMOSTLBASICS20260525'. 	new file:   STLBASICS/BasicMapping.cpp

---
+++ STLBASICS/BasicMapping.cpp
@@ -0,0 +1,16 @@
+/*
+ * BasicMapping.cpp
+ *
+ *  Created on: May 23, 2026
+ *      Author: rodney
+ */
+#include <iostream>
+#include <map>
+#include <string>
+
+
+int main() {
+    std::map<std::string, int> myMap;
+
+    return 0;
+}
